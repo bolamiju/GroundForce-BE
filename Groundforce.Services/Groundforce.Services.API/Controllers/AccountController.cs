701436dc11a09159ccac2aa28cf0104757b39518
Abdulkabir Omotoso <abdulkabir.omotoso@decagon.dev>
2020-10-08 18:15:06 +0100

    Login Implemented

---
+++ Groundforce.Services/Groundforce.Services.API/Controllers/AccountController.cs
@@ -0,0 +1,58 @@
+﻿using Groundforce.Common.Utilities;
+using Groundforce.Services.DTOs;
+using Groundforce.Services.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Threading.Tasks;
+
+namespace Groundforce.Services.API.Controllers
+{
+    [Route("api/v1")]
+    [ApiController]
+    //Account controller
+    public class AccountController : ControllerBase
+    {
+        private readonly ILogger<AccountController> _logger;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly IConfiguration _config;
+        public AccountController(ILogger<AccountController> logger, SignInManager<ApplicationUser> signInManager, UserManager<ApplicationUser> userManager, IConfiguration config)
+        {
+            _logger = logger;
+            _signInManager = signInManager;
+            _userManager = userManager;
+            _config = config;
+        }
+
+        //Log in controller  takes  Email and PassWord
+        [HttpPost("login")]
+        public async Task<IActionResult> LogIn([FromBody] userToLoginDTO model)
+        {
+            var user = await _userManager.FindByEmailAsync(model.Email);
+            if(user == null) return BadRequest("Account not valid");
+
+            if (ModelState.IsValid)
+            {
+                var result = await _signInManager.PasswordSignInAsync(model.Email, model.Password.ToString(), false, false);
+
+                if (result.Succeeded)
+                {
+                    var tokenGet = new TokenGetter();
+                    var tokenValue = tokenGet.GetToken(user, _config);
+                    return Ok(tokenValue);
+                }
+                else
+                {
+                    return Unauthorized("Invalid Email or Pin");
+                }
+            }
+            else
+            {
+                return BadRequest();
+            }
+        }
+    }
+}
---
+++ Groundforce.Services/Groundforce.Services.DTOs/userToLoginDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Groundforce.Services.DTOs
+{
+    public class userToLoginDTO
+    {
+
+        [Required]
+        [DataType(DataType.EmailAddress)]
+
+        public string Email { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public int Password { get; set; }
+    }
+}
