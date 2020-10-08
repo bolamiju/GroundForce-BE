fe8e15721ec712fc27d389ac0b49478d0425336a
Netochukwu Anyankah <nanyankah@gmail.com>
2020-10-08 11:33:52 +0100

    Implement SignUp

--- Groundforce.Services/Groundforce.Services.Data/Migrations/20201008102957_UpdateChanges.Designer.cs
+++ Groundforce.Services/Groundforce.Services.Data/Migrations/20201008102957_UpdateChanges.Designer.cs
@@ -4,14 +4,16 @@ using Groundforce.Services.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Groundforce.Services.Data.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20201008102957_UpdateChanges")]
+    partial class UpdateChanges
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
