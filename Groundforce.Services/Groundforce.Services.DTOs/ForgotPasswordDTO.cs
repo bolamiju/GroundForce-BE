00cc5d1c5843edd7b10ee2804b7b23c0086d14ee
cidospark <cidospark27@gmail.com>
2020-10-25 14:15:43 +0100

    Sign-up, login, forgot-password all refactored

---
+++ Groundforce.Services/Groundforce.Services.DTOs/ForgotPasswordDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Groundforce.Services.DTOs
+{
+    public class ForgotPasswordDTO
+    {
+        [Required]
+        public string PhoneNumber { get; set; }
+        [Required]
+        //[MaxLength(4, ErrorMessage = "Pin must be 4 digits")]
+        public string Pin { get; set; }
+    }
+}
