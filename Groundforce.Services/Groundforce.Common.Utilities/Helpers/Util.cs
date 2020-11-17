f4b13900a2d9944d29e7c8750701a11dea0a8497
cidospark <cidospark27@gmail.com>
2020-11-17 08:43:50 +0100

    Corrected the Email verification model

--- Groundforce.Services/Groundforce.Common.Utilities/Helpers/Util.cs
+++ Groundforce.Services/Groundforce.Common.Utilities/Helpers/Util.cs
@@ -19,47 +19,5 @@ namespace Groundforce.Common.Utilities.Helpers
         }
 
 
-        public static Dictionary<string, byte[]> ConvertToHash(string verificationCode)
-        {
-            byte[] codeSalt, codeHash;
-
-            // convert to a hash value and generate and salt
-            using (var hash = new System.Security.Cryptography.HMACSHA512())
-            {
-                codeSalt = hash.Key;
-                codeHash = hash.ComputeHash(System.Text.Encoding.UTF8.GetBytes(verificationCode));
-            }
-
-            var obj = new Dictionary<string, byte[]>();
-            obj.Add("codeSalt", codeSalt);
-            obj.Add("codeHash", codeHash);
-
-
-            return obj;
-
-        }
-
-        public static bool CompareWithHash(EmailVerification verificationObj, string newCode)
-        {
-            byte[] codeSalt = new byte[0]; byte[] codeHash = new byte[0];
-            bool codeMatch = true;
-
-            // compare the hash
-            //using (var hash = new System.Security.Cryptography.HMACSHA512(verificationObj.CodeSalt))
-            //{
-            //    var hashGenerated = hash.ComputeHash(System.Text.Encoding.UTF8.GetBytes(newCode));
-            //    for (int i = 0; i < hashGenerated.Length; i++)
-            //    {
-            //        if (hashGenerated[i] != verificationObj.CodeHash[i])
-            //        {
-            //            codeMatch = false;
-            //            break;
-            //        }
-            //    }
-            //}
-
-            return codeMatch;
-
-        }
     }
 }
