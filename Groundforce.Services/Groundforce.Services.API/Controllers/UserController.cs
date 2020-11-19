240aebc411310b7a67c9664cb51668c3dbdc043e
22louis2 <otulouisakhigbe@gmail.com>
2020-11-19 19:38:09 +0100

    updated changes

--- Groundforce.Services/Groundforce.Services.API/Controllers/UserController.cs
+++ Groundforce.Services/Groundforce.Services.API/Controllers/UserController.cs
@@ -61,7 +61,7 @@ namespace Groundforce.Services.API.Controllers
                 if (user == null)
                     return NotFound(ResponseMessage.Message("Notfound", errors: $"User with id: {id} was not found"));
 
-                if (_userManager.GetUserId(User) != id ||  !User.IsInRole("Admin"))
+                if (_userManager.GetUserId(User) != id && !User.IsInRole("Admin"))
                     return Unauthorized(ResponseMessage.Message("Unauthorized", errors: $"User must be logged-in or must have admin role"));
 
                 // construct the object
